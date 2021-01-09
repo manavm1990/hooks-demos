7b925ef706ff9dfba452565a15f8d23ca4082d83
Manav Misra <manavm@visionwebsoft.com>
2021-01-09 03:57:14 -0600

    :seedling: Add initial HTML content

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import logo from "./logo.svg";
-
 const App: React.FC = () => (
-  <div className="text-center">
-    <header className="bg-primary flex flex-col align-center justify-center text-10+2 min-h-screen text-white">
-      <img
-        src={logo}
-        className="motion-safe:animate-spin-slow h-40-vmin pointer-events-none"
-        alt="logo"
-      />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-primary"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <>
+    <h1>How fast do you type?</h1>
+    <textarea />
+    <p>
+      Time Remaining: <span>10</span>
+      <button>Start!</button>
+    </p>
+  </>
 );
 
 export default App;
