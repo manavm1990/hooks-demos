7587a5f3c58fb92bd075650e910393da3424ab8c
Manav Misra <manavm@visionwebsoft.com>
2021-01-09 08:50:54 -0600

    :sparkles: Add Helmet 🪖

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Helmet } from "react-helmet";
+
 const App: React.FC = () => (
   <>
+    <Helmet>
+      <html lang="en-us" />
+      <title>How Fast Do You Type?</title>
+      <meta name="description" content="A very simple typing speed test" />
+      <meta name="theme-color" content="#00b800" />
+    </Helmet>
+
     <h1>How fast do you type?</h1>
     <textarea />
     <p>
